7d03bb63c4a9510ddfe68a30b956c7763e1537fb
Oliver Hale <oliver.a.hale@gmail.com>
2024-01-16 13:28:15 +1100

    refactor api code and add structuring for the post to ticket types

--- field_update_poc/main.py
+++ field_update_poc/main.py
@@ -3,19 +3,21 @@ import os
 import pandas as pd
 import requests
 import logging
+from modules.utilities.api import postToAPI, getFromAPI
 
-from config import API_TOKEN
+from field_update_poc.modules.utilities.config import API_TOKEN
 
 INPUT_FOLDER = './input'
 
 
 #Hardcoded system field exclusions for field info
 excluded_fields = ['Summary', 'Details', 'Category', 'Impact', 'Urgency', 'Asset']
+exc_list = {"Summary":2, "Details":3, "Category": 5, "Impact":165,"Urgency":166,"Asset":4}
+
 
 #Template for the API request to update fields
 templ = {"usage": 1,"inputtype": "0","characterlimittype": "0","visibility": "1","tab_id": "0"}
 json_template = json.dumps(templ)
-
 logging.basicConfig(filename='api_log.txt', level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
 
 #ingests a hardcoded csv or excel file
@@ -63,53 +65,30 @@ def structureData(df):
     # print(df_row_list[1])
     #Michael helped write this part
 
-#Sends data to the api via a hardcoded url
-#TODO: make dynamic, and allow the user to enter their bearer info
-def sendToAPI(data):
-    url = 'https://oliverhale.internal.halopsa.com/api/fieldinfo'
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {getAccessToken()}'
-    }
-    response = requests.post(url, headers=headers, json=data)
 
-    if response.status_code in range(200, 299):
-        print("Data sent successfully!")
-        print(f"Response: {response.status_code}")
-    else:
-        print(f"Error sending data. Status code: {response.status_code}, Response: {response.text}")
-        
-    logging.info(f"Status code: {response.status_code}\n Result: {response.text}")
+
     
-          
-
-#Unimplemented
-#TODO: Implement
-def getAccessToken():
-    # Implement logic to obtain and return the access token using your client ID and secret
-    # This would involve making a request to the auth server
-
-    # token_url = 'https://example.halo.com/auth'
-    # token_data = {'grant_type': 'client_credentials', 'client_id': CLIENT_ID, 'client_secret': CLIENT_SECRET}
-    # response = requests.post(token_url, data=token_data)
-    # return response.json().get('access_token')
-
-    # For testing purposes, you can replace this with a hardcoded token (not recommended for production):
-    return ''
-
-
 def main():
     df = importData()
     fields = df[~df['label'].isin(excluded_fields)].drop(columns=['Ticket Type', 'Mandatory (Y/N)', 'Agent Only'])    
     fieldJson = structureData(fields)
     
-    for row in fieldJson:
-        print('-------------------')
-        print(row)
-        print('-------------------')
-        
-    sendToAPI(fieldJson)
+    # for row in fieldJson:
+    #     print('-------------------')
+    #     print(row)
+    #     print('-------------------')
+    
+    #Post the new fields to the API
+    # postToAPI(fieldJson, 'fieldinfo')
+    
+    #Add the new fields to the ticket types
+    
+    
+    
+    
+
 
 
 if __name__ == "__main__":
     main()
+
---
+++ field_update_poc/modules/utilities/api.py
@@ -0,0 +1,69 @@
+import requests
+import logging
+from field_update_poc.modules.utilities.config import API_TOKEN
+from main import excluded_fields
+
+
+#Sends data to the api via a hardcoded url
+#TODO: make dynamic, and allow the user to enter their bearer info
+def postToAPI(data, endpoint):
+    url = f'https://oliverhale.internal.halopsa.com/api/{endpoint}'
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {getAccessToken()}'
+    }
+    response = requests.post(url, headers=headers, json=data)
+    showResponseStatus(response)
+        
+    logging.info(f"====================\POST PERFORMED\n===================\nStatus code: {response.status_code}\n Result: {response.text}\n")
+
+
+#Makes a GET request to an endpoint
+def getFromAPI(endpoint):
+    url = f'https://oliverhale.internal.halopsa.com/api/{endpoint}'
+    headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {getAccessToken()}'
+        }
+    response = requests.get(url, headers=headers)
+    showResponseStatus(response)
+
+    logging.info(f"====================\nGET REQUEST PERFORMED\n===================\nStatus code: {response.status_code}\n")
+    
+    return response
+
+def showResponseStatus(response):
+    if response.status_code in range(200,299):
+        print("Request successful")
+        print(f"Response: {response.status_code}")
+    else:
+        print(f"Error sending data. Status code: {response.status_code}, Response: {response.text}")
+    
+
+#Unimplemented
+#TODO: Implement
+def getAccessToken():
+    # Implement logic to obtain and return the access token using your client ID and secret
+    # This would involve making a request to the auth server
+
+    # token_url = 'https://example.halo.com/auth'
+    # token_data = {'grant_type': 'client_credentials', 'client_id': CLIENT_ID, 'client_secret': CLIENT_SECRET}
+    # response = requests.post(token_url, data=token_data)
+    # return response.json().get('access_token')
+
+    # For testing purposes, you can replace this with a hardcoded token (not recommended for production):
+    return API_TOKEN
+
+def retrieveFieldIDs(fields):
+    response = getFromAPI('fieldinfo')
+        
+    if response.status_code in (200,299):
+        json_data=response.json()
+        field_ids = [entry['id'] for entry in json_data if entry.get("label") in fields]
+    else:
+        print("No values to parse")
+        return
+    
+    return field_ids
+        
+        
